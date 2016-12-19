bfade2998cde76b50c3f62e8c9ff627337aefe51
Maxime <maxime@linux-2s4u.site>
2016-12-20 00:01:48 +0100

    Veuillez saisir le message de validation pour vos modifications. Les lignes erreur de null pointor exception

--- icasa.light.user.command/src/main/java/org/example/follow/me/manager/command/FollowMeManagerCommandImpl.java
+++ icasa.light.user.command/src/main/java/org/example/follow/me/manager/command/FollowMeManagerCommandImpl.java
@@ -1,5 +1,7 @@
 package org.example.follow.me.manager.command;
 
+import java.rmi.UnexpectedException;
+
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Requires;
@@ -8,45 +10,56 @@ import org.example.follow.me.api.IlluminanceGoal;
 
 import fr.liglab.adele.icasa.command.handler.Command;
 import fr.liglab.adele.icasa.command.handler.CommandProvider;
-
+ 
 //Define this class as an implementation of a component :
 @Component
 //Create an instance of the component
-@Instantiate
+@Instantiate(name = "follow.me.mananger.command")
 //Use the handler command and declare the command as a command provider. The
 //namespace is used to prevent name collision.
 @CommandProvider(namespace = "followme")
 public class FollowMeManagerCommandImpl {
-
+ 
     // Declare a dependency to a FollowMeAdministration service
     @Requires
     private FollowMeAdministration m_administrationService;
-
-
+ 
+ 
     /**
      * Felix shell command implementation to sets the illuminance preference.
      *
      * @param goal the new illuminance preference ("SOFT", "MEDIUM", "FULL")
      */
-
+ 
     // Each command should start with a @Command annotation
     @Command
     public void setIlluminancePreference(String goal) {
         // The targeted goal
-        //TODO : Fix the init
-        IlluminanceGoal illuminanceGoal = IlluminanceGoal.MEDIUM;
-
-        // TODO : Here you have to convert the goal string into an illuminance
-        // goal and fail if the entry is not "SOFT", "MEDIUM" or "HIGH"
-
-        //call the administration service to configure it :
-        m_administrationService.setIlluminancePreference(illuminanceGoal);
+        IlluminanceGoal illuminanceGoal = null;
+ 
+        try
+        {
+	    	if(goal.equals("SOFT"))
+	        	illuminanceGoal=IlluminanceGoal.SOFT;
+	        else if(goal.equals("MEDIUM"))
+	        	illuminanceGoal=IlluminanceGoal.MEDIUM;
+	    	else if(goal.equals("FULL"))
+	    		illuminanceGoal=IlluminanceGoal.FULL;
+	    	else{
+	    		throw new UnexpectedException("Illuminance goal UNKWNOWN");
+	    		
+	    	}
+        }catch(UnexpectedException e){
+        	e.printStackTrace();
+        }
+    	if(illuminanceGoal != null){
+    		m_administrationService.setIlluminancePreference(illuminanceGoal);
+    	}
     }
-
+ 
     @Command
     public void getIlluminancePreference(){
-        //TODO : implement the command that print the current value of the goal
-        System.out.println("The illuminance goal is "); //...
+        System.out.println("The illuminance goal is "+ this.m_administrationService.getIlluminancePreference()); //...
     }
-
+ 
 }
